c46a1641d66b32f9af5b45dd5da256e46f8c1faa
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2025-10-24 09:41:20 +0700

    ulang upload image logo umy

--- app/src/main/java/uipertama.kt
+++ app/src/main/java/uipertama.kt
@@ -1,3 +1,5 @@
+import java.lang.reflect.Modifier
+
 @Composable
 fun ActivitasPertama(modifier: Modifier){
     Column(
@@ -16,5 +18,46 @@ fun ActivitasPertama(modifier: Modifier){
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height( height = 25.dp))
-    )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding( all = 12.dp)
+                    colors = CardDefaults.cardColors(
+                    containerColor = Color .DarkGrey
+                    )
+        ) {
+                Row() {
+                    val gambar = painterResource( id = R.drawable.logo_umy)
+                    Image(
+                        painter = gambar,
+                        contentDescription = null,
+                        modifier = Modifier.size( size = 100.dp)
+                    )
+                    }
+                Row() {
+                    modifier = Modifier
+                        .size( size = 100.dp)
+                        .padding( all = 5.dp)
+                    )
+                    Spacer(modifier = Modifier .width( width = 30.dp))
+                    Column() {
+                        Text(
+                            text = stringResources( id = R.string.nama),
+                            fontSize = 30.sp,
+                            fontFamily = FontFamily.Cusive,
+                            color = Color.White,
+                            modifier = Modifier.padding(top = 15.dp)
+                        )
+                        Text(
+                            text = stringResource( id = R.string.alamat)
+                            fontSize = 20.sp,
+                            color = Color.Yellow,
+                            modifier = Modifier.padding(top = 10.dp
+                            )
+                        )
+
+                    }
+                }
+
+            }
     }
